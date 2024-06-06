b52df64086225197f064d0813094023a7ff2ce89
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-06 19:47:41 +0530

    updated the docker compose

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import UploadCard from './components/PostUploadCard';
 import PostDetails from './components/PostDetails';
 import OtherProfilePage from './pages/OtherProfilePage';
 
-axios.defaults.baseURL = 'http://design-flow-server:3000/';
+axios.defaults.baseURL = 'http://server:3000/';
 axios.defaults.withCredentials = true;
 
 function App() {
